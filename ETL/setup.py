3f793474ba783ecb1cbc3e15e9d900aaa243f50f
queirozale <queirozalessandro1@gmail.com>
2021-03-10 15:08:53 -0300

    Project structure

---
+++ ETL/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Jeco Data Engineering',
+    author='Alessandro Queiroz',
+    license='MIT',
+)
